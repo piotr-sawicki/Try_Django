87b8874573ded4d6de138701e8369567b2bdd4d3
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-10-18 20:50:23 +0200

    14 - rendering HTML document added

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,32 +4,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    output_str = f'<h1>Home View</h1><br> ' \
-                 f'<h2>Request: {str(request)[1:-1]} </h2><br>' \
-                 f'<h2>User: {request.user}</h2>'
-
-    return HttpResponse(output_str)
+    return render(request, 'home.html', {})
 
 
 def contact_view(request, *args, **kwargs):
-    output_str = f'<h1>Contact View</h1><br> ' \
-                 f'<h2>Request: {str(request)[1:-1]} </h2><br>' \
-                 f'<h2>User: {request.user}</h2>'
-
-    return HttpResponse(output_str)
+    return render(request, 'contact.html')
 
 
 def about_view(request, *args, **kwargs):
-    output_str = f'<h1>About View</h1><br> ' \
-                 f'<h2>Request: {str(request)[1:-1]} </h2><br>' \
-                 f'<h2>User: {request.user}</h2>'
-
-    return HttpResponse(output_str)
-
-
-def social_view(request, *args, **kwargs):
-    output_str = f'<h1>Social View</h1><br> ' \
-                 f'<h2>Request: {str(request)[1:-1]} </h2><br>' \
-                 f'<h2>User: {request.user}</h2>'
-
-    return HttpResponse(output_str)
+    return render(request, 'about.html')
