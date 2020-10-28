f4667f76a1e1bee34ad11a675ca2c0761c19f26f
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-10-28 21:50:37 +0100

    23 - creating the object based on ModelForm

--- src/products/models.py
+++ src/products/models.py
@@ -6,4 +6,5 @@ class Product(models.Model):
     title =       models.CharField(max_length=120) # max_length is required
     description = models.TextField(blank=True, null=True)
     price =       models.DecimalField(decimal_places=2, max_digits=10000)
+    feature =   models.BooleanField(default=False)
     summary =     models.TextField()
