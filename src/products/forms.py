e18058683559d4d973e98e0fd6cae8fd9a425761
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-11-01 14:18:14 +0100

    26 - Widgets in Forms

--- src/products/forms.py
+++ src/products/forms.py
@@ -15,8 +15,22 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Your title"
+            }
+        )
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "new-class-name two",
+                "rows": 5,
+                "cols": 20,
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
     feature = forms.BooleanField()
     summary = forms.CharField()
